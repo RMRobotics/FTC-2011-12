a5e51f40955ceee959ec559211bdbf428843e941
makecakenotwar97@gmail.com <makecakenotwar97@gmail.com>
2011-11-24 21:59:14 -0500

    Commented in possible dead reckoned + ultrasonic drive path from RED base 	Requires ultrasonic measurer to be placed facing the back and 	lined up with the back of the robot 	This could work without the ultrasonic thing, but it'll probably 	be off because we would have to guess the offsets and the bowling 	ball will probably change our course 	Ultrasonic might hit an obstacle, so we'll have to have 	code for that...

--- FTC_2011-12_Auto.c
+++ FTC_2011-12_Auto.c
@@ -17,13 +17,58 @@
 #include "Autonomous.h"
 
 task main(){
-  //Code: drive(auto_command,time);
-  //replace auto_command and time with values necessary (if you don't replace them, code won't run)
-  //auto_command values are: "rpoint", "lpoint", "up", "down", "rswing", "lswing", "rswingback", "lswingback"
-  //time values is the time, in milliseconds, you want the robot to be doing the action specified in auto_command
+	//Code: drive(auto_command,time);
+	//replace auto_command and time with values necessary (if you don't replace them, code won't run)
+	//auto_command values are: "rpoint", "lpoint", "up", "down", "rswing", "lswing", "rswingback", "lswingback"
+	//time values is the time, in milliseconds, you want the robot to be doing the action specified in auto_command
 
-  //example of a autonomous program (runs):
-  drive("up", 10000);      //move forward 10000 milliseconds (10sec)
-  drive("lpoint", 1000);   //left point turn for 1000 milliseconds (1sec)
-  drive("up", 10000);      //move forward again for 10000 milliseconds (10sec)
+	string program_type="Red";
+	int offset_back = 0;
+	int offset_right = 0;
+	int robot_length = 0; //Make this a constant?
+
+	if (program_type="Red") {
+		//1) Measure offset from walls
+		//If possible start robot sideways to reduce time it takes to measure
+		//Ultrasonic measurer should be placed facing the back with the edge lined up with the robot's back
+		//Convert offset distance into milliseconds
+		offset_back = -1;
+		offset_right = -1;
+		
+		//2) Move forward ((Four Feet Seven Inches)-offset_back)
+		
+		//3) Point turn -90 degrees
+		
+		//4) Move forward ((Four Feet Eight Inches)-(the robot's length)-offset_right)
+		
+		//5) Point turn -33.7 degrees
+		
+		//6) Move forward ((Nine Feet Two Inches)-(the robot's length))
+		
+		//7) Pause 1 second
+		
+		//8) Move backward (Three Inches)
+		
+		//9) Point turn 33.7 degrees
+		
+		//10) Measure offset from RED base wall (should be from 5' - 7')
+		offset_right = -1;
+		
+		//11) Move backward until (offset=(Two Feet Six Inches))
+		
+		//12) Point Turn 90 degrees
+		
+		//13) Measure offset
+		offset_back = -1;
+		
+		//14) Move until (offset=(Ten Feet Six Inches)-(half the robot's length)
+		
+		//15) Point Turn 90 Degrees
+		
+		//16) Move forward (Five Feet Six Inches)
+	}
+  
+	drive("up", 10000);      //move forward 10000 milliseconds (10sec)
+	drive("lpoint", 1000);   //left point turn for 1000 milliseconds (1sec)
+	drive("up", 10000);      //move forward again for 10000 milliseconds (10sec)
 }
